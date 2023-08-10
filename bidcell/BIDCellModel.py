9dd576b5cac1c43e7c37ba2baf25caf5f37536b3
Nick <nickrrrr@gmail.com>
2023-08-10 10:13:19 +1000

    add skeleton

---
+++ bidcell/BIDCellModel.py
@@ -0,0 +1,24 @@
+"""BIDCellModel class module"""
+
+
+class BIDCellModel:
+
+    def __init__(self) -> None:
+        pass
+
+    def preprocess() -> None:
+        pass
+
+    def train() -> None:
+        pass
+
+    def predict() -> None:
+        pass
+
+
+if __name__ == "__main__":
+
+    model = BIDCellModel()
+    model.preprocess()
+    model.train()
+    model.predict()
