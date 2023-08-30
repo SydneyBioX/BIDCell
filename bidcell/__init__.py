330852d6962b4b78c382648724e3474e6c2140bf
Nicholas Robertson <nickrrrr@gmail.com>
2023-08-30 14:00:13 +1000

    init outline

---
+++ bidcell/__init__.py
@@ -0,0 +1,7 @@
+from bidcell.BIDCellModel import BIDCellModel
+
+__all__ = ["BIDCellModel"]
+__doc__ = """
+BIDCell - biologically-informed deep learning for cell segmentation of subcelluar spatial transcriptomics data
+==============================================================================================================
+"""
